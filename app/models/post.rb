4139dd9920f928f8bfa3211e167321115e4ce56e
Nobuo <akiyamajyoni0922@gmail.com>
2018-10-24 00:58:47 +0900

    各モデルのアソシエーションの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  has_many    :tapispot_scores, dependent:  :destroy
+  has_many    :images,          dependent:  :destroy
+  belongs_to  :user
 end
--- app/models/tapispot_score.rb
+++ app/models/tapispot_score.rb
@@ -1,2 +1,3 @@
 class TapispotScore < ApplicationRecord
+  belongs_to    :post
 end
