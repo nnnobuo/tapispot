07cf1d54f7572582fffaed085d099984e6dddd34
Nobuo <akiyamajyoni0922@gmail.com>
2018-10-24 01:43:43 +0900

    投稿機能実装ver1.0

--- app/controllers/tapispot_controller.rb
+++ app/controllers/tapispot_controller.rb
@@ -1,5 +1,20 @@
 class TapispotController < ApplicationController
   def index
+    @post = Post.new
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:shop_name, :post_title, :post_body).merge(user_id: current_user.id)
   end
 end
